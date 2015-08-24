63c33923d586988d37189be5da9a6f1b6bf1808e
Jim Menard <jim.menard@chloeandisabel.com>
2015-08-24 10:09:11 -0400

    Added ItemSummary GET request.

---
+++ lib/active_fulfillment/models/dotcom_distribution/item_summary.rb
@@ -0,0 +1,43 @@
+module ActiveFulfillment
+  module DotcomDistribution
+
+    class ItemSummary
+
+      include ::ActiveModel::Model
+
+      attr_accessor :sku,
+                    :description,
+                    :last_receipt_date,
+                    :upc_number,
+                    :vendor_items
+
+      def self.response_from_xml(xml)
+        success = true, message = '', hash = {}, records = []
+        doc = Nokogiri.XML(xml)
+        doc.remove_namespaces!
+
+        doc.xpath("//item_info").each do |el|
+          hash[:sku] = el.at('.//sku').try(:text)
+          hash[:description] = el.at('.//item_description').try(:text)
+          hash[:last_receipt_date] = el.at('.//last_receipt_date').try(:text)
+          hash[:upc_number] = el.at('.//upc_num').try(:text)
+          hash[:vendor_items] = el.xpath('.//vendor_items//vendor_item').collect do |item|
+            VendorItem.new(cross_ref: item.at('.//vendor_cross_ref').try(:text))
+          end
+          records << ItemSummary.new(hash)
+        end
+        if records.length > 0
+          return Response.new(false, '', {data: records})
+        end
+        nil
+      end
+    end
+
+    class VendorItem
+
+      include ::ActiveModel::Model
+
+      attr_accessor :cross_ref
+    end
+  end
+end
---
+++ test/unit/models/dotcom_distribution/item_summary_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class ItemSummaryTest < Minitest::Test
+  include ActiveFulfillment::Test::Fixtures
+  include ActiveFulfillment::DotcomDistribution
+
+  def setup
+    ActiveFulfillment::Base.mode = :test
+
+    xml = <<-SQL
+      <?xml version="1.0" encoding="UTF-8"?>
+      <response xmlns="http://dcd/datacontracts/iteminfo" xmlns:i="http://www.w3.org/2001/XMLSchema- instance">
+        <items_info xmlns:a="http://schemas.datacontract.org/2004/07/DCDAPIService"> <a:item_info>
+          <a:item_description>Cassie Cardigan</a:item_description>
+          <a:last_receipt_date>7/27/2012 2:53:00 PM</a:last_receipt_date>
+          <a:sku>360-103053-DEW-M</a:sku>
+          <a:upc_num />
+          <a:vendor_items>
+            <a:vendor_item>
+              <a:vendor_cross_ref>846255082961</a:vendor_cross_ref> </a:vendor_item>
+            </a:vendor_items>
+          </a:item_info>
+        </items_info>
+      </response>
+    SQL
+
+    @item = ItemSummary.response_from_xml(xml).data.first
+  end
+
+
+  def test_get_order_deserialization
+    assert_equal @item.sku, '360-103053-DEW-M'
+    assert_equal @item.description, 'Cassie Cardigan'
+    assert_equal @item.last_receipt_date, '7/27/2012 2:53:00 PM'
+    assert_equal @item.vendor_items.length, 1
+    assert_equal @item.vendor_items[0].cross_ref, '846255082961'
+  end
+
+end
