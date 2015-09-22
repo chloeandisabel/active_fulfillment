58d6b80a822e556e73416038423d51a26aff8a57
Jim Menard <jim.menard@chloeandisabel.com>
2015-09-22 16:09:48 -0400

    One more prevention of Hash reuse.

--- lib/active_fulfillment/models/dotcom_distribution/get_purchase_order.rb
+++ lib/active_fulfillment/models/dotcom_distribution/get_purchase_order.rb
@@ -13,29 +13,28 @@ module ActiveFulfillment
                     :po_items
 
       def self.response_from_xml(xml)
-        success = true, message = '', hash = {}, records = []
+        success = true, message = '', records = []
         doc = Nokogiri.XML(xml)
         doc.remove_namespaces!
         doc.xpath("//purchase-order").each do |el|
-          hash[:po_number] = el.at('.//po-number').try(:text)
-          hash[:po_status] = el.at('.//po-status').try(:text)
-          hash[:dcd_po_number] = el.at('.//dcd-po-number').try(:text)
-          hash[:po_date] = el.at('.//po-date').try(:text)
-          hash[:priority_date] = el.at('.//priority-date').try(:text)
-          hash[:expected_date] = el.at('.//expected-date').try(:text)
-          hash[:po_items] = el.xpath('.//po-items//po-item').collect do |item|
-            PurchaseOrderItem.new(sku: item.at('.//sku').try(:text),
-                                  item_description: item.at('.//item-description').try(:text),
-                                  expected_qty: item.at('.//expected-qty').try(:text).to_i,
-                                  received_qty: item.at('.//received-qty').try(:text).to_i,
-                                  open_qty: item.at('.//open-qty').try(:text).to_i,
-                                  status: item.at('.//status').try(:text),
-                                  po_line_num: item.at('.//po-line-num').try(:text).to_i,
-                                  style: item.at('.//style').try(:text),
-                                  color: item.at('.//color').try(:text),
-                                  size: item.at('.//size').try(:text))
-          end
-          records << GetPurchaseOrder.new(hash)
+          records << GetPurchaseOrder.new(po_number: el.at('.//po-number').try(:text),
+                                          po_status: el.at('.//po-status').try(:text),
+                                          dcd_po_number: el.at('.//dcd-po-number').try(:text),
+                                          po_date: el.at('.//po-date').try(:text),
+                                          priority_date: el.at('.//priority-date').try(:text),
+                                          expected_date: el.at('.//expected-date').try(:text),
+                                          po_items: el.xpath('.//po-items//po-item').collect { |item|
+                                            PurchaseOrderItem.new(sku: item.at('.//sku').try(:text),
+                                                                  item_description: item.at('.//item-description').try(:text),
+                                                                  expected_qty: item.at('.//expected-qty').try(:text).to_i,
+                                                                  received_qty: item.at('.//received-qty').try(:text).to_i,
+                                                                  open_qty: item.at('.//open-qty').try(:text).to_i,
+                                                                  status: item.at('.//status').try(:text),
+                                                                  po_line_num: item.at('.//po-line-num').try(:text).to_i,
+                                                                  style: item.at('.//style').try(:text),
+                                                                  color: item.at('.//color').try(:text),
+                                                                  size: item.at('.//size').try(:text))
+                                          })
         end
         Response.new(true, '', {data: records})
       end
