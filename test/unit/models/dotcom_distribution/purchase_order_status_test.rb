a5e1416ddde18aae53adc93203ba358bede6672c
Jim Menard <jim.menard@chloeandisabel.com>
2016-01-06 14:14:28 -0500

    Dotcom PO XML elements: like_this, not like-this.

--- test/unit/models/dotcom_distribution/purchase_order_status_test.rb
+++ test/unit/models/dotcom_distribution/purchase_order_status_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class PurchaseOrderStatusTest < Minitest::Test
   include ActiveFulfillment::Test::Fixtures
@@ -6,31 +6,30 @@ class PurchaseOrderStatusTest < Minitest::Test
 
   def setup
     ActiveFulfillment::Base.mode = :test
-    xml = xml_fixture('dotcom_distribution/purchase_order_status')
+    xml = xml_fixture("dotcom_distribution/purchase_order_status")
     @po = GetPurchaseOrder.response_from_xml(xml).data.first
   end
 
 
   def test_get_order_deserialization
-    assert_equal 'PO1234', @po.po_number
-    assert_equal 'O', @po.po_status
-    assert_equal 'DCD-PO-1234', @po.dcd_po_number
-    assert_equal '2011-07-10', @po.po_date
-    assert_equal '2011-07-11', @po.priority_date
-    assert_equal '2011-07-12', @po.expected_date
+    assert_equal "D002", @po.po_number
+    assert_equal "A", @po.po_status
+    assert_equal "260664570", @po.dcd_po_number
+    assert_equal "12/29/2015", @po.po_date
+    assert_equal "12/29/2015", @po.priority_date
+    assert_equal "12/29/2015 12:00:00 AM", @po.expected_date
 
-    assert_equal 1, @po.po_items.length
+    assert_equal 2, @po.po_items.length
     item = @po.po_items[0]
-    assert_equal 'the-sku', item.sku
-    assert_equal 'the description', item.item_description
-    assert_equal 12, item.expected_qty
-    assert_equal 11, item.received_qty
-    assert_equal 1, item.open_qty
-    assert_equal 'O', item.status
+    assert_equal "E224SQ", item.sku
+    assert_equal "Minaret Light Rose Teardrop Ea", item.item_description
+    assert_equal 500, item.expected_qty
+    assert_equal 500, item.received_qty
+    assert_equal 0, item.open_qty
+    assert_equal "A", item.status
     assert_equal 1, item.po_line_num
-    assert_equal 'style text', item.style
-    assert_equal 'color text', item.color
-    assert_equal 'size text', item.size
+    assert_equal "color text", item.color
+    assert_equal "", item.style
+    assert_equal "", item.size
   end
-
 end
