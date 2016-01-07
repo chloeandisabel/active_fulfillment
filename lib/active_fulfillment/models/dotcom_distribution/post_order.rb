e960ae11940ee1c60b7a09d0efd6a118993c9a25
Jim Menard <jim.menard@chloeandisabel.com>
2016-01-07 14:22:09 -0500

    Max lengths and non-ASCII filtering

--- lib/active_fulfillment/models/dotcom_distribution/post_order.rb
+++ lib/active_fulfillment/models/dotcom_distribution/post_order.rb
@@ -1,4 +1,5 @@
 require_relative 'nil_injector'
+require_relative 'string_normalizer'
 
 module ActiveFulfillment
   module DotcomDistribution
@@ -20,6 +21,7 @@ module ActiveFulfillment
     class PostOrder
       include Model
       include ::ActiveFulfillment::DotcomDistribution::NilInjector
+      include ::ActiveFulfillment::DotcomDistribution::StringNormalizer
 
       attr_accessor :order_number,
                     :order_date,
@@ -187,42 +189,70 @@ module ActiveFulfillment
 
       def order_to_xml(xml)
         xml.order do
-          xml.send(:"order-number", self.order_number)
-          xml.send(:"order-date", self.order_date)
-          xml.send(:"ship_date", self.ship_date)
-          xml.send(:"ship-method", self.ship_method)
-          xml.send(:"ship_via", self.ship_via, inject_nil(self.ship_via))
-          xml.send(:"drop-ship", self.drop_ship, inject_nil(self.drop_ship))
-          xml.send(:"special-instructions", self.special_instructions, inject_nil(self.special_instructions))
-          xml.send(:"special-messaging", self.special_messaging, inject_nil(self.special_messaging))
-          xml.send(:"invoice-number", self.invoice_number, inject_nil(self.invoice_number))
-          xml.send(:"declared-value", self.declared_value, inject_nil(self.declared_value))
-          xml.send(:"ok-partial-ship", self.ok_partial_ship, inject_nil(self.ok_partial_ship))
-          xml.send(:"cancel-date", self.cancel_date, inject_nil(self.cancel_date))
+          xml.send(:"order-number", normalize(self.order_number, :order_number))
+          xml.send(:"order-date", normalize(self.order_date, :date))
+          xml.send(:"ship_date", normalize(self.ship_date, :date))
+          xml.send(:"ship-method", normalize(self.ship_method, :ship_method))
+          xml.send(:"ship_via", normalize(self.ship_via, :ship_via),
+                   inject_nil(self.ship_via))
+          xml.send(:"drop-ship", normalize(self.drop_ship, :drop_ship),
+                   inject_nil(self.drop_ship))
+          xml.send(:"special-instructions", normalize(self.special_instructions, :special_instructions),
+                   inject_nil(self.special_instructions))
+          xml.send(:"special-messaging", normalize(self.special_messaging, :special_messaging),
+                   inject_nil(self.special_messaging))
+          xml.send(:"invoice-number", normalize(self.invoice_number, :invoice_number),
+                   inject_nil(self.invoice_number))
+          xml.send(:"declared-value", self.declared_value,
+                   inject_nil(self.declared_value))
+          xml.send(:"ok-partial-ship", normalize(self.ok_partial_ship, :ok_partial_ship),
+                   inject_nil(self.ok_partial_ship))
+          xml.send(:"cancel-date", normalize(self.cancel_date, :date),
+                   inject_nil(self.cancel_date))
           xml.send(:"total-tax", self.total_tax)
           xml.send(:"total-shipping-handling", self.total_shipping_handling)
           xml.send(:"total-discount", self.total_discount)
           xml.send(:"total-order-amount", self.total_order_amount)
-          xml.send(:"po-number", self.po_number, inject_nil(self.po_number))
-          xml.send(:"salesman", self.salesman, inject_nil(self.salesman))
-          xml.send(:"credit-card-number", self.credit_card_number, inject_nil(self.credit_card_number))
-          xml.send(:"credit-card-expiration", self.credit_card_expiration, inject_nil(self.credit_card_expiration))
-          xml.send(:"ad-code", self.ad_code, inject_nil(self.ad_code))
-          xml.send(:"continuity-flag", self.continuity_flag, inject_nil(self.continuity_flag))
-          xml.send(:"freight-terms", self.freight_terms, inject_nil(self.freight_terms))
-          xml.send(:"department", self.department, inject_nil(self.department))
-          xml.send(:"pay-terms", self.pay_terms, inject_nil(self.pay_terms))
-          xml.send(:"tax-percent", self.tax_percent, inject_nil(self.tax_percent))
-          xml.send(:"asn-qualifier", self.asn_qualifier, inject_nil(self.asn_qualifier))
-          xml.send(:"gift-order-indicator", self.gift_order_indicator, inject_nil(self.gift_order_indicator))
-          xml.send(:"order-source", self.order_source, inject_nil(self.order_source))
-          xml.send(:"promise-date", self.promise_date, inject_nil(self.promise_date))
-          xml.send(:"third-party-account", self.third_party_account, inject_nil(self.third_party_account))
-          xml.send(:"priority", self.priority, inject_nil(self.priority))
-          xml.send(:"retail-department", self.retail_department, inject_nil(self.retail_department))
-          xml.send(:"retail-store", self.retail_store, inject_nil(self.retail_store))
-          xml.send(:"retail-vendor", self.retail_vendor, inject_nil(self.retail_vendor))
-          xml.send(:"pool", self.pool, inject_nil(self.pool))
+          xml.send(:"po-number", normalize(self.po_number, :po_number),
+                   inject_nil(self.po_number))
+          xml.send(:"salesman", normalize(self.salesman, :salesman),
+                   inject_nil(self.salesman))
+          xml.send(:"credit-card-number", normalize(self.credit_card_number, :credit_card_number),
+                   inject_nil(self.credit_card_number))
+          xml.send(:"credit-card-expiration", normalize(self.credit_card_expiration, :credit_card_expiration),
+                   inject_nil(self.credit_card_expiration))
+          xml.send(:"ad-code", normalize(self.ad_code, :ad_code),
+                   inject_nil(self.ad_code))
+          xml.send(:"continuity-flag", normalize(self.continuity_flag, :continuity_flag),
+                   inject_nil(self.continuity_flag))
+          xml.send(:"freight-terms", normalize(self.freight_terms, :freight_terms),
+                   inject_nil(self.freight_terms))
+          xml.send(:"department", normalize(self.department, :department),
+                   inject_nil(self.department))
+          xml.send(:"pay-terms", normalize(self.pay_terms, :pay_terms),
+                   inject_nil(self.pay_terms))
+          xml.send(:"tax-percent", self.tax_percent,
+                   inject_nil(self.tax_percent))
+          xml.send(:"asn-qualifier", normalize(self.asn_qualifier, :asn_qualifier),
+                   inject_nil(self.asn_qualifier))
+          xml.send(:"gift-order-indicator", normalize(self.gift_order_indicator, :gift_order_indicator),
+                   inject_nil(self.gift_order_indicator))
+          xml.send(:"order-source", normalize(self.order_source, :order_source),
+                   inject_nil(self.order_source))
+          xml.send(:"promise-date", normalize(self.promise_date, :date),
+                   inject_nil(self.promise_date))
+          xml.send(:"third-party-account", normalize(self.third_party_account, :third_party_account),
+                   inject_nil(self.third_party_account))
+          xml.send(:"priority", normalize(self.priority, :priority),
+                   inject_nil(self.priority))
+          xml.send(:"retail-department", normalize(self.retail_department, :retail_department),
+                   inject_nil(self.retail_department))
+          xml.send(:"retail-store", normalize(self.retail_store, :retail_store),
+                   inject_nil(self.retail_store))
+          xml.send(:"retail-vendor", normalize(self.retail_vendor, :retail_vendor),
+                   inject_nil(self.retail_vendor))
+          xml.send(:"pool", normalize(self.pool, :pool),
+                   inject_nil(self.pool))
 
           add_billing_or_shipping_information(xml, self.billing_information, 'billing')
           add_billing_or_shipping_information(xml, self.shipping_information, 'shipping')
@@ -231,7 +261,8 @@ module ActiveFulfillment
 
           xml.send(:"custom-fields") do
             for i in 1..5
-              xml.send(:"custom-field-#{i}", self.custom_fields[i], inject_nil(self.custom_fields[i]))
+              xml.send(:"custom-field-#{i}", normalize(self.custom_fields[i], :custom_field),
+                       inject_nil(self.custom_fields[i]))
             end
           end
 
@@ -246,66 +277,79 @@ module ActiveFulfillment
 
       def add_billing_or_shipping_information(xml, address, pref = 'billing')
         xml.send(:"#{pref}-information") do
-          xml.send(:"#{pref}-customer-number", address.customer_number, inject_nil(address.customer_number))
+          xml.send(:"#{pref}-customer-number", normalize(address.customer_number, :customer_number),
+                   inject_nil(address.customer_number))
           xml.send(:"#{pref}-name") do
-            xml.cdata address.name
+            xml.cdata normalize(address.name, :name)
           end
           xml.send(:"#{pref}-company", inject_nil(address.company)) do
-            xml.cdata address.company if address.company
+            xml.cdata normalize(address.company, :company) if address.company
           end
           xml.send(:"#{pref}-address1") do
-            xml.cdata address.address1
+            xml.cdata normalize(address.address1, :address)
           end
           xml.send(:"#{pref}-address2", inject_nil(address.address2)) do
-            xml.cdata address.address2 if address.address2
+            xml.cdata normalize(address.address2, :address) if address.address2
           end
           xml.send(:"#{pref}-address3", inject_nil(address.address3)) do
-            xml.cdata address.address3 if address.address3
+            xml.cdata normalize(address.address3, :address) if address.address3
           end
           xml.send(:"#{pref}-city") do
-            xml.cdata address.city
+            xml.cdata normalize(address.city, :city)
           end
-          xml.send(:"#{pref}-state", address.state)
-          xml.send(:"#{pref}-zip", address.zip)
-          xml.send(:"#{pref}-country", address.country, inject_nil(address.country))
-          xml.send(:"#{pref}-iso-country-code", address.iso_country_code) if pref == 'shipping'
-          xml.send(:"#{pref}-phone", address.phone, inject_nil(address.phone))
-          xml.send(:"#{pref}-email", address.email, inject_nil(address.email))
+          xml.send(:"#{pref}-state", normalize(address.state, :state))
+          xml.send(:"#{pref}-zip", normalize(address.zip, :zip))
+          xml.send(:"#{pref}-country", normalize(address.country, :country),
+                   inject_nil(address.country))
+          if pref == 'shipping'
+            xml.send(:"#{pref}-iso-country-code", normalize(address.iso_country_code, :country))
+          end
+          xml.send(:"#{pref}-phone", normalize(address.phone, :phone),
+                   inject_nil(address.phone))
+          xml.send(:"#{pref}-email", normalize(address.email, :email),
+                   inject_nil(address.email))
         end
       end
 
       def add_store_information(xml, address)
         xml.send(:"store-information") do
           xml.send(:"store-name", inject_nil(address.try(:name))) do
-            xml.cdata address.name if address.try(:name)
+            xml.cdata normalize(address.name, :name) if address.try(:name)
           end
           xml.send(:"store-address1", inject_nil(address.try(:address1))) do
-            xml.cdata address.try(:address1)
+            xml.cdata normalize(address.try(:address1), :address)
           end
           xml.send(:"store-address2", inject_nil(address.try(:address2))) do
-            xml.cdata address.try(:address2)
+            xml.cdata normalize(address.try(:address2), :address)
           end
           xml.send(:"store-city", inject_nil(address.try(:city))) do
-            xml.cdata address.try(:city)
+            xml.cdata normalize(address.try(:city), :address)
           end
-          xml.send(:"store-state", address.try(:state), inject_nil(address.try(:state)))
-          xml.send(:"store-zip", address.try(:zip), inject_nil(address.try(:zip)))
-          xml.send(:"store-country", address.try(:country), inject_nil(address.try(:country)))
-          xml.send(:"store-phone", address.try(:phone), inject_nil(address.try(:phone)))
+          xml.send(:"store-state", normalize(address.try(:state), :state),
+                   inject_nil(address.try(:state)))
+          xml.send(:"store-zip", normalize(address.try(:zip), :zip),
+                   inject_nil(address.try(:zip)))
+          xml.send(:"store-country", normalize(address.try(:country), :country),
+                   inject_nil(address.try(:country)))
+          xml.send(:"store-phone", normalize(address.try(:phone), :phone),
+                   inject_nil(address.try(:phone)))
         end
       end
 
       def add_line_item(xml, line_item, index)
         xml.send(:"line-item") do
-          xml.send(:"sku", line_item.sku)
+          xml.send(:"sku", normalize(line_item.sku, :sku))
           xml.send(:"quantity", line_item.quantity)
           xml.send(:"tax", line_item.tax)
           xml.send(:"price", line_item.price)
           xml.send(:"shipping-handling", line_item.shipping_handling)
-          xml.send(:"client-item", line_item.client_item, inject_nil(line_item.client_item))
-          xml.send(:"line-number", line_item.line_number)
-          xml.send(:"gift-box-wrap-quantity", line_item.gift_box_wrap_quantity, inject_nil(line_item.gift_box_wrap_quantity))
-          xml.send(:"gift-box-wrap-type", line_item.gift_box_wrap_type, inject_nil(line_item.gift_box_wrap_type))
+          xml.send(:"client-item", normalize(line_item.client_item, :client_item),
+                   inject_nil(line_item.client_item))
+          xml.send(:"line-number", normalize(line_item.line_number, :line_number))
+          xml.send(:"gift-box-wrap-quantity", line_item.gift_box_wrap_quantity,
+                   inject_nil(line_item.gift_box_wrap_quantity))
+          xml.send(:"gift-box-wrap-type", normalize(line_item.gift_box_wrap_type, :gift_box_wrap_type),
+                   inject_nil(line_item.gift_box_wrap_type))
         end
       end
     end
